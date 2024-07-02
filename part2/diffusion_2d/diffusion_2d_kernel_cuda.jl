53873dd7993ebca3191902f3843f6b626d8eb8d6
Ludovic Raess <ludovic.rass@gmail.com>
2024-07-02 15:50:05 +0200

    Align

--- part2/diffusion_2d/diffusion_2d_kernel_cuda.jl
+++ part2/diffusion_2d/diffusion_2d_kernel_cuda.jl
@@ -15,7 +15,7 @@ function diffusion_step!(C2, C, D, dt, dx, dy)
     ix = (blockIdx().x-1) * blockDim().x + threadIdx().x;
     iy = (blockIdx().y-1) * blockDim().y + threadIdx().y;
     @inbounds C2[ix+1, iy+1] = C[ix+1, iy+1] - dt * ((@qx(ix+1, iy+1) - @qx(ix, iy+1)) / dx +
-                                           (@qy(ix+1, iy+1) - @qy(ix+1, iy)) / dy)
+                                                     (@qy(ix+1, iy+1) - @qy(ix+1, iy)) / dy)
     return
 end
 
