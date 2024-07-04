4c675759550b10935795ce8e616300d0c9d6ffc8
Carsten Bauer <crstnbr@gmail.com>
2024-07-04 08:33:35 +0200

    helper scripts

---
+++ help/gpunode.sh
@@ -0,0 +1,3 @@
+# Request an entire GPU node for interactive usage (you'll end up with a shell on the compute node)
+# Run as: sh gpunode.sh
+salloc --nodes 1 --qos interactive --time 01:00:00 --constraint gpu --gpus 4 --account=ntrain
