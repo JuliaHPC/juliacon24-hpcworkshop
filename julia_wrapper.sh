d060e6b886f71d321a0d814334497e06ed79cc8e
Carsten Bauer <crstnbr@gmail.com>
2024-07-09 11:58:39 +0200

    Update julia_wrapper.sh

--- julia_wrapper.sh
+++ julia_wrapper.sh
@@ -4,5 +4,8 @@
 ml use /global/common/software/nersc/n9/julia/modules
 ml julia
 
+# Set depot path (only for the workshop)
+export JULIA_DEPOT_PATH=$SCRATCH/.julia:/global/common/software/ntrain1/.julia
+
 # Pass on all arguments to julia
 exec julia "${@}"
