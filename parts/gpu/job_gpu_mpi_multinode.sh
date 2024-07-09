4d8f4d5b9c802c889b0f06c7427779b4ebb384f0
Johannes Blaschke <jpblaschke@lbl.gov>
2024-07-08 18:41:05 -0700

    add slurm scripts to exercise

--- parts/gpu/job_gpu_mpi_multinode.sh
+++ parts/gpu/job_gpu_mpi_multinode.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 #SBATCH --time=00:05:00
 #SBATCH --nodes=4
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gpus-per-task=1
 #SBATCH --constraint=gpu
-#SBATCH --gpu=4
 #SBATCH --account=ntrain1
 #SBATCH --output=slurm_gpu_mpi_multinode.out
 #SBATCH --qos=regular
+
+# pin to closest NIC to GPU
+export MPICH_OFI_NIC_POLICY=GPU
 
 # Load julia
 ml use /global/common/software/nersc/n9/julia/modules
 ml julia
 
-mpiexecjl --project -n 4 julia diffusion_2d_cuda_mpi.jl
+mpiexecjl --project julia diffusion_2d_cuda_mpi.jl
